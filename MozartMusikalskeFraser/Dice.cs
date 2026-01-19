41e303535f997a9c94335e587d68c1c00f0afe67
Christian Herskind Sløk <cherskindslok@gmail.com>
2026-01-19 12:16:58 +0100

    Add initial build artifacts for MozartMusikalskeFraser.dll and refint

--- MozartMusikalskeFraser/Dice.cs
+++ MozartMusikalskeFraser/Dice.cs
@@ -1,23 +1,68 @@
-namespace MozartMusikalskeFraser;
+using System;
 
-public static class Dice
+class MozartValse
 {
-    // Slår én seks-sidet terning (1-6)
-    public static int Roll6()
+    static void Main()
     {
-        return Random.Shared.Next(1, 7);
+        Random rnd = new Random();
+
+        Console.WriteLine("Vælg instrument (Piano, Mbira, Flute-harp eller Clarinet):");
+        string input = Console.ReadLine();
+
+        string instrument = "Piano"; // standard
+        if (input == "Piano")
+        {
+            instrument = "piano";
+        }
+        else if (input == "Mbira")
+        {
+            instrument = "mbira";
+        }
+        else if (input == "Flute-harp")
+        {
+            instrument = "flute-harp";
+        }
+        else if (input == "Clarinet")
+        {
+            instrument = "clarinet";
+        }
+        else
+        {
+            Console.WriteLine("Ugyldigt valg, bruger Piano.");
+        }
+
+        string basePath = $"C:\\Users\\u50716\\Downloads\\mozart\\mozart\\{instrument}\\";
+        Console.WriteLine($"Valgt instrument: {instrument}");
+        Console.WriteLine("Spiller vers...");
+        
+        // -------- Menuet --------
+        for (int frase = 0; frase < 16; frase++)
+        {
+            int sum = rnd.Next(1, 7) + rnd.Next(1, 7);
+            string file = $"{basePath}minuet{frase}-{sum}.wav";
+
+            PlaySound(file);
+        }
+
+        // -------- Trio --------
+        for (int frase = 0; frase < 16; frase++)
+        {
+            int dice = rnd.Next(1, 7);
+            string file = $"{basePath}trio{frase}-{dice}.wav";
+
+            PlaySound(file);
+        }
+
+        Console.WriteLine("Vers færdig");
     }
 
-    // Generisk terning: 1..sides (default 6)
-    public static int Roll(int sides = 6)
+    static void PlaySound(string path)
     {
-        if (sides < 1) sides = 1;
-        return Random.Shared.Next(1, sides + 1);
-    }
-
-    // Slår to seks-sidede terninger og returnerer summen (2-12)
-    public static int Roll2d6()
-    {
-        return Roll6() + Roll6();
+        System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+        {
+            FileName = path,
+            UseShellExecute = true
+        });
+        System.Threading.Thread.Sleep(2000);
     }
 }
